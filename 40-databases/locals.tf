8af9c723ddb75dfe099093fbbc58bcc830234d73
JyothiYellam <jyothiym23@gmail.com>
2026-08-12 21:01:48 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -8,5 +8,6 @@ locals {
     #we get public subnet id in 1a AZ
     database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 }
 
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -6,3 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
+variable "zone_id" {
+    default = "Z01562281MA12MBQ767HT"
+}
+
+variable "domain_name" {
+    default = "jyothiy.online"
+}
