1cd327eaa4d9a6e6c3c2bf9de0d3a21561b9ee6f
JyothiYellam <jyothiym23@gmail.com>
2026-08-13 13:38:06 +0530

    changes

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -78,3 +78,44 @@ resource "terraform_data" "bootstrap_redis" {
         ]
     }
 }
+
+
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    subnet_id = local.database_subnet_ids
+    vpc_security_group_ids = [local.mysql_sg_id]
+    
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-mysql"
+        },
+        local.common_tags
+    )
+}
+
+resource "terraform_data" "bootstrap" {
+    triggers_replace = [
+        aws_instance.mysql.id
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.mysql.private_ip 
+    } 
+
+    provisioner "file" {
+        source = "bootstrap.sh" #local file path 
+        destination = "/tmp/bootstrap.sh" #Destination path on remote machine
+    }
+
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/bootstrap.sh",
+            "sudo sh /tmp/bootstrap.sh mysql"
+        ]
+    }
+}
