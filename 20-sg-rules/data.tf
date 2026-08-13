bf1419bd2f2e4dc170481c5e5f2f5e8f308191bf
JyothiYellam <jyothiym23@gmail.com>
2026-08-13 13:49:29 +0530

    changes

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -19,7 +19,7 @@ data "aws_ssm_parameter" "redis_sg_id" {
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-    name = "/${var.project}/${var.environment}/mysql_sg_id/"
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
 
