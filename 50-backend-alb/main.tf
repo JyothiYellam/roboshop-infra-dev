9f6ac014ba321c687f3116168d3c77e3aa5ee66d
JyothiYellam <jyothiym23@gmail.com>
2026-08-16 12:21:08 +0530

    changes

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -28,8 +28,10 @@ resource "aws_lb_listener" "http" {
         content_type =  "text/html"
         message_body = "<h1>Hi, I am from HTTP Backend ALB</h1>"
         status_code = "200"
+        }
     }
 }
+
 
  resource "aws_route53_record" "www" {
     zone_id = var.zone_id
