230c7e5cc1af1cde48917debdb83fbe728d34256
JyothiYellam <jyothiym23@gmail.com>
2026-08-20 13:43:55 +0530

    changes

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,9 @@
 locals {
-   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-   ami_id = data.aws_ami.devops.id   
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    ami_id = data.aws_ami.devops.id   
     private_subnet_ids =  split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     common_tags = {
         Project =  var.project
         Environment = var.environment
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -55,3 +55,138 @@ resource "aws_ami_from_instance" "catalogue" {
   )
 }
 
+resource "aws_lb_target_group" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = local.vpc_id
+  deregistration_delay = 60
+  health_check = {
+    healthy_threshold = 20
+    interval  = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold =  3
+  }
+} 
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
+
+  # once autoscaling sees less traffic, it will terminate the instance
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  update_default_version = true
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+    )
+  }
+  tag_specifications {
+    resource_type = "volume"
+    tags = merge(
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
+    )
+  }
+
+  tags  = merge(
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      }
+    )
+}
+
+ resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project}-${var.environment}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  
+  vpc_zone_identifier       = [local.private_subnet_id]
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  dynamic "tag" {
+    for_each = merge(
+        {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+    )
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  # with in 15min autoscaling should be successful
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name   =     aws_autoscaling_group.catalogue.name
+  name                     =    "${var.project}-${var.environment}-catalogue"
+  policy_type       =    "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 7.0
+  }
+}
+
+resource "aws_lb_listner_rule" "catalogue" {
+  listener_arn = local.backend_alb_listener_rn
+  priority = 10
+  action {
+    type = "forward"
+    target_group_arn  = aws_lb_target_group.catalogue.arn
+  }
+  condition {
+    host_header{
+      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+resource "terraform_data" "catalogue_delete" {
+    triggers_replace = [
+      aws_instance.catalogue.id
+    ]
+    depends_on = [aws_autoscaling_policy.catalogue]
+    provisioner "local-exec" {
+      command = "aws_ec2 terminate-instances ${aws_instance.catalogue.id}"
+    }
+}
+
