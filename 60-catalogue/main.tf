f898847c497f06b4d1a3243b410806243073561a
JyothiYellam <jyothiym23@gmail.com>
2026-08-20 19:00:31 +0530

    changes

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -186,7 +186,7 @@ resource "terraform_data" "catalogue_delete" {
     ]
     depends_on = [aws_autoscaling_policy.catalogue]
     provisioner "local-exec" {
-      command = "aws_ec2 terminate-instances --instance_ids ${aws_instance.catalogue.id}"
+      command = "aws ec2 terminate-instances --instance_ids ${aws_instance.catalogue.id}"
     }
 }
 
