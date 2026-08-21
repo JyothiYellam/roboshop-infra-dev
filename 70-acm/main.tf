2f7489d861d10d2fcfb4715f9693c33a262e33ba
JyothiYellam <jyothiym23@gmail.com>
2026-08-21 21:31:45 +0530

    changes

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -5,8 +5,7 @@ resource "aws_acm_certificate" "roboshop" {
   tags = merge(
     {
         Name = "${var.project}-${var.environment}-${var.domain_name}"
-    },
-    local.common_tags
+    }
   )
 
   lifecycle {
